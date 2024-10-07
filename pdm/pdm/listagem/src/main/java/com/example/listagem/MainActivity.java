9568be6912feaae997a75b5f2b515d908f47e5f2
lezio09 <144951527+lezio09@users.noreply.github.com>
2024-10-07 11:04:27 -0300

    Add files via upload
    Listagem

---
+++ pdm/pdm/listagem/src/main/java/com/example/listagem/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.listagem;
+
+import static com.example.listagem.R.*;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    String[] nomes = new String[]{"Picareta", "Jhones Dark", "Camili", "Daniela", "Todynho"};
+    ListView listView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(layout.activity_main);
+        listView=findViewById(R.id.ListView);
+
+        ArrayAdapter arrayAdapter= new ArrayAdapter(this,
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1, nomes);
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                        Toast.makeText(getApplicationContext(),
+                                Integer.toString(i) +nomes[i],
+                                Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
+
+    }
+}
